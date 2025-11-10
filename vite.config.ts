64a52e9c05a4ab0a52e3ec076eb85d617ba4c531
isaacleong12345 <chenglongleong@gmail.com>
2025-11-10 23:13:23 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
-    },
-  },
-  root: path.resolve(__dirname, "client"),
+  plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "dist"), // Remove /public
-    emptyOutDir: true,
-  },
-  server: {
-    port: 3000,
-    host: true,
-  },
+    outDir: "dist"
+  }
 });
